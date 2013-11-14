57d123833257bfc317e254e2878192eabbca818a
tuvistavie <tuvistavie@gmail.com>
2013-11-14 11:22:49 +0900

    Update auth for get requests.

--- src/aws_credentials.erl
+++ src/aws_credentials.erl
@@ -1,16 +1,37 @@
 -module(aws_credentials).
 
 -include_lib("aws_client.hrl").
+-include_lib("http_wrapper.hrl").
 
--export([generate_signature_key/1]).
+-import(http_wrapper, [param/2]).
+
+-export([
+  generate_signature_key/1,
+  generate_auth_params/1
+]).
+
+-spec get_credentials(string(), string(), string(), string()) -> string().
+get_credentials(Key, DateStamp, RegionName, ServiceName) ->
+  string:join([Key, DateStamp, RegionName, ServiceName, "aws4_request"], "/").
+
+-spec generate_auth_params(aws_client()) -> [param()].
+generate_auth_params(Client) ->
+    { Key, DateStamp, RegionName, ServiceName, Signature, Now } = generate_signature_key(Client),
+    [http_wrapper:param("X-Amz-Algorithm", "AWS4-HMAC-SHA256"),
+     http_wrapper:param("X-Amz-Date", date_util:to_iso8601(Now)),
+     http_wrapper:param("X-Amz-SignedHeaders", "content-type;host"),
+     http_wrapper:param("X-Amz-Signature", Signature),
+     http_wrapper:param("X-Amz-Credential", get_credentials(Key, DateStamp, RegionName, ServiceName))].
 
 -spec generate_signature_key(aws_client()) -> string().
 generate_signature_key(Client) ->
   Key = Client#aws_client.credentials#aws_credentials.secret_key,
-  DateStamp = binary_to_list(iso8601:format(now())),
-  RegionName = "foo",
-  ServiceName = "bar",
-  generate_signature_key(Key, DateStamp, RegionName, ServiceName).
+  Now = now(),
+  DateStamp = date_util:to_iso8601_date(Now),
+  RegionName = Client#aws_client.configuration#aws_configuration.region,
+  ServiceName = Client#aws_client.service,
+  Signature = generate_signature_key(Key, DateStamp, RegionName, ServiceName),
+  { Key, DateStamp, RegionName, ServiceName, Signature, Now }.
 
 
 -spec generate_signature_key(string(), string(), string(), string()) -> string().
@@ -21,3 +42,4 @@ generate_signature_key(Key, DateStamp, RegionName, ServiceName) ->
   SigningKey = hmac:hmac256(ServiceKey, "aws4_request"),
 
   string:to_lower(hmac:hexlify(SigningKey)).
+
---
+++ src/date_util.erl
@@ -0,0 +1,28 @@
+-module(date_util).
+
+-export([
+  to_iso8601/1,
+  to_iso8601_date/1
+]).
+
+-type datetime() :: tuple(Date::calendar:date(),
+                          Time::calendar:time()).
+-type timestamp() :: tuple(MegaSecs::integer(),
+                           Secs::integer(),
+                           MicroSecs::integer()).
+
+-spec to_iso8601(datetime() | timestamp()) -> string().
+to_iso8601({_,_,_}=Timestamp) ->
+  to_iso8601(calendar:now_to_datetime(Timestamp));
+to_iso8601({{Y,Mo,D}, {H,Mn,_}}) ->
+    FmtStr = "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0BZ",
+    IsoStr = io_lib:format(FmtStr, [Y, Mo, D, H, Mn]),
+    binary_to_list(list_to_binary(IsoStr)).
+
+-spec to_iso8601_date(datetime() | timestamp()) -> string().
+to_iso8601_date({_,_,_}=Timestamp) ->
+  to_iso8601_date(calendar:now_to_datetime(Timestamp));
+to_iso8601_date({{Y,Mo,D}, {_,_,_}}) ->
+    FmtStr = "~4.10.0B~2.10.0B~2.10.0B",
+    IsoStr = io_lib:format(FmtStr, [Y, Mo, D]),
+    binary_to_list(list_to_binary(IsoStr)).
